acf6b9c02fbb2311eeff030e4d16db9a0c174b86
坂本 樹理亜 <j.sakamoto@bravesoft.co.jp>
2018-03-14 10:54:04 +0900

    Cut down unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <p>{{ msg }}</p>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
